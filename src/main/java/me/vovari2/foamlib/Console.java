25b85c0e0191e16927959d9ebd3a7c52aa52ce07
Vovari2 <vovari4ek2@gmail.com>
2025-05-21 00:18:58 +0300

    Warning fix

--- src/main/java/me/vovari2/foamlib/Console.java
+++ src/main/java/me/vovari2/foamlib/Console.java
@@ -2,7 +2,6 @@ package me.vovari2.foamlib;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public abstract class Console {
 
